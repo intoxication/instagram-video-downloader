05a6e1cc903e8e5377eeaac6ffb3395aa82e78e0
intoxication <intoxication.me@gmail.com>
2024-05-26 21:16:00 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,13 +3,13 @@ import { DM_Sans as FontSans } from "next/font/google";
 
 import "@/styles/globals.css";
 
-import { Navbar } from "@/components/navbar";
-import { Footer } from "@/components/footer";
+import { Navbar } from "/components/navbar";
+import { Footer } from "/components/footer";
 
-import { ThemeProvider } from "@/components/providers/theme-provider";
-import { ReactQueryProvider } from "@/components/providers/react-query-provider";
+import { ThemeProvider } from "/components/providers/theme-provider";
+import { ReactQueryProvider } from "/components/providers/react-query-provider";
 
-import { cn } from "@/lib/utils";
+import { cn } from "/lib/utils";
 
 const fontSans = FontSans({
   subsets: ["latin"],
